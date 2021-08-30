94d3c2773586056212186899b8cec1d9f4ffe217
Charles Hernandez <charlesh4758@gmail.com>
2021-08-29 19:51:17 -0500

    Insert questions and answer buttons into quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,43 +4,53 @@ var elAnswers = document.getElementById("answers");
 var startBtn = document.querySelector(".start-button");
 var correctAns = document.querySelector(".correct");
 var wrongAns = document.querySelector(".wrong");
+var scoreName = document.querySelector(".names-col");
+var scoreNum = document.querySelector(".scores-col");
+var nameEl = document.createElement("h2");
+var scoreEl = document.createElement("h2");
+var questionCount = 0;
 var timerCount;
-var correctCount;
-var wrongCount;
+var correctCount = 0;
+var wrongCount = 0;
+
 var questionsArr = [
-  { question: "question 1",
+  { question: "What is the index number of the first item of an array?",
     answers: [
-      { a: "12",
-        b: "13",
-        c: "14",
-      }],
-      correctAnswer: "a",
-      question: "question 2",
-    answers: [
-      { a: "12",
-        b: "13",
-        c: "14",
-      }],
-      correctAnswer: "b",
-      question: "question 3",
-      answers: [
-        { a: "12",
-          b: "13",
-          c: "14",
-        }],
-    correctAnswer: "c",
-      question: "question 4",
-    answers: [
-      { a: "12",
-        b: "13",
-        c: "14",
-      }],
-      correctAnswer: "d",
+        "1",
+        "0",
+        "2",
+      ],
+      correctAnswer: "0",
     },
+      { question: "What type of container is used to hold an array?",
+    answers: [
+        "Parantheses",
+        "Curly Braces",
+        "Square Brackets",
+      ],
+      correctAnswer: "Square Brackets",
+    },
+      { question: "What style of case is used in JavaScript?",
+      answers: [
+        "UPPERCASE",
+        "lowercase",
+        "camelCase",
+      ],
+      correctAnswer: "camelCase",
+    },
+      { question: "question 4",
+      answers: [
+        "dan",
+        "dap",
+        "dab",
+      ],
+      correctAnswer: "dan",
+    }
 ];
 
 function startGame() {
-  timerCount = 5;
+  timerCount = 10;
+  
   console.log("go!");
   var interval = setInterval(function(){
     timerCount --;
@@ -55,41 +65,70 @@ function startGame() {
       clearInterval(interval);
       startBtn.textContent = "Try again?";
       startBtn.disabled = false;
+      questionCount = 0;
       endGame();
     }
-    // displayQuestion();
-
   }, 1000);
+  displayQuestion();
 };
 
 // Start Button activating game
 startBtn.addEventListener("click", startGame);
 
-
 function displayQuestion(){
-  var currentQuestion = questionsArr[currentQuestionIndex];
-  var questions = currentQuestion.question;
+  var currentQuestion = questionsArr[questionCount];
+  var question = currentQuestion.question;
   var answers = currentQuestion.answers
   // set question text to current question
-  // clear out previous buttons
+  elQuestion.textContent = question;
   // loop through answer to create buttons
-  // for (i=0, i>answers.length, i++){
-
+  for (let i=0; i < answers.length; i++){
+    var answerBtn = document.createElement("button");
+    answerBtn.textContent = answers[i];
+    elAnswers.appendChild(answerBtn);
+    console.log("answer");
+  }
+  //at the end of questions - end game
+  // if (questionCount > 4){
+  //   ;
   // }
-}
+};
 
 function endGame (){
   console.log("It's over!");
+  //store quiz score
+  localStorage.setItem("correct", correctCount);
+  //add prompt to record initials + store initials
+  var highScoreName = prompt("You got " + correctCount + " correct! Enter your initials for the High Score Board!");  
+  localStorage.setItem("name", highScoreName);
 };
-
-
-
+//clicking a button in the answer element
 elAnswers.addEventListener("click", function(event){
-  var answer = event.target.getAttribute
-  if(answer) {
-    // record answer
+  event.stopPropagation();
+  var answer = elAnswers.querySelector("button");
+  //get text from selected button
+  var answerClick = answer.getText;
+  console.log(answerClick);
+  //check if right or wrong answer
+  if (answerClick === questionsArr.correctAnswer){
+    correctCount ++;
+    correctAns.textContent = correctCount;
+    console.log("correct!");
+  } else {
+    wrongCount ++;
+    wrongAns.textContent = wrongCount;
+    console.log("wrong :/");
+  };
+  //clear out previous buttons
+  elAnswers.innerHTML = "";
+  console.log("bye buttons");
+  // go to next question
+  questionCount++;
+  displayQuestion();
+});
 
-    // go to next question
+nameEl.textContent = localStorage.getItem("name");
+scoreEl.textContent = localStorage.getItem("correct");
 
-  }
-})
+nameEl.appendChild(nameEl);
+scoreEl.appendChild(scoreEl);
